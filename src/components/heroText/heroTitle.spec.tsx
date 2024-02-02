4f016290f9281018f307ff9995b65a6bb63f6520
Vahid Akhtar <akhtarvahid543@gmail.com>
2024-02-02 17:46:15 +0530

    coverage threshold: Missing line 5

--- src/components/heroText/heroTitle.spec.tsx
+++ src/components/heroText/heroTitle.spec.tsx
@@ -2,10 +2,10 @@ import { render, screen } from '@testing-library/react';
 import HeroTitle from './heroTitle';
 
 describe('Hero component tests', () => {
-    test('sub components', () => {
+    test('sub components: Test1', () => {
         render(<HeroTitle title="Hero" />);
         const linkElement = screen.getByText('Hero');
         expect(linkElement).toBeInTheDocument();
-      });    
+      });     
 })
     
--- src/components/heroText/heroTitle.tsx
+++ src/components/heroText/heroTitle.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const HeroTitle: React.FC<{title: string}> = ({ title }) => {
     return (
-      <div>{title}</div>
+      <div>{title ? title : 'Vahid'}</div>
     )
 }
 export default HeroTitle;
