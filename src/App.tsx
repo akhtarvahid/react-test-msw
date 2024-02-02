85d5b001e957cc131dda82f15c2b7436bb1019cd
Vahid Akhtar <akhtarvahid543@gmail.com>
2024-02-02 17:34:56 +0530

    coverage configuration

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,9 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import HeroTitle from './components/heroText/heroTitle';
 
 function App() {
   return (
     <div className="App" data-testid="app">
-      
       <HeroTitle title="Hero Title" />
     </div>
   );
