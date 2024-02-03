9c08ffc27ca3fe8b5fbcb7306ddb5bfeadd4af0d
Vahid Akhtar <akhtarvahid543@gmail.com>
2024-02-04 00:06:48 +0530

    listing component rendered

--- src/components/listing/Listing.tsx
+++ src/components/listing/Listing.tsx
@@ -1,10 +1,27 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 const Listing: React.FC = () => {
-  return (
-    <div>
-        <h1>Users</h1>
-    </div>
-  )
+    const [users, setUsers] = useState([]);
+    const [error, setError] = useState('');
+
+    useEffect(() => {
+        fetch('https://jsonplaceholder.typicode.com/users')
+            .then(res => res.json())
+            .then(res => setUsers(res))
+            .catch(err => setError(err))
+    }, [])
+
+    console.log(users)
+
+    return (
+        <div>
+            <h1>Users</h1>
+            <div>
+                {!error && users && users.map((user: any) =>
+                    <p style={{ background: 'lightslategray' }} key={user.id}>{user.name}</p>
+                )}
+            </div>
+        </div>
+    )
 }
 export default Listing;
