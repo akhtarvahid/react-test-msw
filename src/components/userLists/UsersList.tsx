3f5c5ab940283f5436c0a0c42c0e9aff5a81fa00
Vahid Akhtar <akhtarvahid543@gmail.com>
2024-02-08 01:00:44 +0530

    post request with msw

--- src/components/userLists/UsersList.tsx
+++ src/components/userLists/UsersList.tsx
@@ -5,6 +5,7 @@ const UsersList: React.FC = () => {
     const [users, setUsers] = useState<object[]>([]);
     const [fetching, setFetching] = useState<boolean>(false);
     const [error, setError] = useState<null | string>('');
+    const [input, setInput] = useState('');
 
     useEffect(() => {
         setFetching(true);
@@ -12,7 +13,6 @@ const UsersList: React.FC = () => {
             .then(res => res.json())
             .then(res => {
                 setUsers(res);
-                //localStorage.setItem('names', res);
                 setFetching(false);
             })
             .catch(() => setError('Something happened'))
@@ -29,8 +29,20 @@ const UsersList: React.FC = () => {
             <ul>
                 {!error ? users && users.map((user: any, i) =>
                     <li style={{ background: 'lightslategray' }} key={i}>{user.name}</li>
-                ): <div>Something happened</div>}
+                ) : <div>Something happened</div>}
             </ul>
+            <input type='text' placeholder='Enter something' onChange={(e) => setInput(e.target.value)}/>
+            <button onClick={(e) => {
+                e.preventDefault();
+                fetch('https://jsonplaceholder.typicode.com/posts', {
+                    method: "POST",
+                    headers: {
+                        "Content-type": "application/json"
+                    },
+                    body: JSON.stringify(input)
+                })
+                .then()
+            }}>Send</button>
         </div>
     )
 }
---
+++ src/components/userLists/__tests__/UsersList.spec.tsx
@@ -0,0 +1,51 @@
+import { render, screen } from '@testing-library/react';
+import UsersList from '../UsersList';
+
+describe('UsersList component tests', () => {
+    
+    test('UsersList: Test 1', () => {
+        render(<UsersList />);
+        const header = screen.getByRole('heading');
+        expect(header).toBeInTheDocument();
+    });
+    test('UsersList: Test 2', async () => {
+        render(<UsersList />);
+        const listItem = await screen.findAllByRole('listitem');
+        expect(listItem).toHaveLength(10);
+    });
+
+    test('UsersList: error Test 3', async () => {
+        
+        render(<UsersList />);
+
+        // worker.use(
+        //     http.get('https://jsonplaceholder.typicode.com/users', () => {
+        //         return new HttpResponse('Something happened', { status: 401 })
+        //     })
+        // )
+
+        //expect(await screen.findAllByRole('listitem')).not.toBeInTheDocument()
+        // expect(await screen.findByText('No users found')).toBeInTheDocument()
+
+        // const error = screen.queryByText('Something happened');
+        // expect(error).not.toBeInTheDocument();
+    });
+
+    // test('UsersList: Check response Test 3', async () => {
+    //     render(<UsersList />);
+    //     const heading = screen.getByRole('heading', { name: 'Fetching latest posts...'});
+    //     // 🕗 Wait for the posts request to be finished.
+    //     await waitFor(() => expect(heading).not.toBeInTheDocument());
+
+        
+    //     const getfromLocalStorage = localStorage.getItem('names')?.split(',') || [];
+    //     expect(mockData[0]).toEqual(fakeUserResponse[0]);
+    // });
+
+    test('UsersList: Error Test 4', async () => {
+        
+        // render(<UsersList />);
+        // const error = await screen.findByText('Something happened');
+        // expect(error).toBeInTheDocument();
+    });
+})
