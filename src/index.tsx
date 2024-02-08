638defb42c8de111956d8740fae9fbd11604e7a5
Vahid Akhtar <akhtarvahid543@gmail.com>
2024-02-09 00:22:47 +0530

    cleanup..

--- src/index.tsx
+++ src/index.tsx
@@ -5,23 +5,15 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-
-async function enableMocking() {
-  const { worker } = await import('./browser-integration/mock/browser');
-  return worker.start()
-}
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
 
-
-enableMocking().then(() => {
-  root.render(
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
-  );
-})
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
