58c8a94b182ea5c96cd5456041c31844449d6ec9
Vahid Akhtar <akhtarvahid543@gmail.com>
2024-02-22 00:09:42 +0530

    heroTitle removed

--- src/components/heroText/heroTitle.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-
-const HeroTitle: React.FC<{title?: string}> = ({ title }) => {
-    return (
-      <h1>{title ? title : 'Vahid'}</h1>
-    )
-}
-export default HeroTitle;
