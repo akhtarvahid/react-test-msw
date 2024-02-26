80db598190d34fd1b61367a120897fc968e92d10
Vahid Akhtar <akhtarvahid543@gmail.com>
2024-02-27 00:17:50 +0530

    usersApp whole app tested

--- src/components/users-app/UserList.tsx
+++ src/components/users-app/UserList.tsx
@@ -16,7 +16,7 @@ function UserList({ users }: any) {
           <th>Email</th>
         </tr>
       </thead>
-      <tbody>{renderedUsers}</tbody>
+      <tbody data-testid="users">{renderedUsers}</tbody>
     </table>
   );
 }
