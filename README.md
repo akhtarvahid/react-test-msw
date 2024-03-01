bed2f9c1aa8ddb5a2f39ade44b5e8c8fcc66fc99
Vahid Akhtar <akhtarvahid543@gmail.com>
2024-03-02 01:05:53 +0530

    feature-1 init

--- README.md
+++ README.md
@@ -1,15 +1,6 @@
 # react-test-msw
 Testing React components using msw
 
-<div style="display: none">
-<a href="https://fakestoreapi.com/" style="display: none">Free ecommerce-website</a>
-
-<a href="https://rickandmortyapi.com/documentation/#introduction" style="display: none">Movie paginated data</a>
-</div>
-
-
-
-
 
 Run 
 
--- jest.config.js
+++ jest.config.js
@@ -3,4 +3,14 @@ module.exports = {
     testEnvironmentOptions: {
         customExportConditions: [''],
     },
+    // Refer to -> https://github.com/testing-library/eslint-plugin-testing-library/blob/main/docs/rules/prefer-presence-queries.md
+    // Suggests to use correct assertion methods
+    rules: {
+        'testing-library/prefer-presence-queries': [
+            'error',
+            { absence: false, presence: true },
+        ],
+    },
+    // Enable👇 If you want to use -> jest.setup.js
+   //setupFilesAfterEnv: ['<rootDir>/jest.setup.js']
 }
